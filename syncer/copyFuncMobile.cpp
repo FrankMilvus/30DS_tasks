1d24d0fbb17c90a08883a934bc2416ccdab2765e
FrankMilvus <frankmilvus@gmail.com>
2025-06-25 23:20:32 +0300

    work, copy to connected device and any other local place

--- syncer/copyFuncMobile.cpp
+++ syncer/copyFuncMobile.cpp
@@ -1,12 +1,13 @@
-/*he simplest way to copy a file in C++ on Windows 11
- (including mobile/tablet devices) is to use the Windows
-  API function CopyFile() or CopyFile2().*/
 #include <windows.h>
 #include <string>
 #include <iostream>
 
-int copyFileMobile(const std::wstring &sourceFile, const std::wstring &destFile)
+int copyFileMobile(const std::string &sourceFileStr, const std::string &destFileStr)
 {
+    // Convert narrow strings to wide strings
+    std::wstring sourceFile(sourceFileStr.begin(), sourceFileStr.end());
+    std::wstring destFile(destFileStr.begin(), destFileStr.end());
+
     // Copy file (fails if destination exists)
     if (CopyFileW(sourceFile.c_str(), destFile.c_str(), TRUE))
     {
@@ -15,7 +16,19 @@ int copyFileMobile(const std::wstring &sourceFile, const std::wstring &destFile)
     }
     else
     {
-        std::wcerr << L"Mobile copying: Failed to copy file. Error: " << GetLastError() << L"\n";
+        DWORD error = GetLastError();
+        std::wcerr << L"Mobile copying: Failed to copy file. Error: " << error << L"\n";
+
+        // Specific error handling
+        if (error == ERROR_FILE_EXISTS)
+        {
+            std::wcerr << L"Mobile copying: Target file already exists\n";
+        }
+        else if (error == ERROR_PATH_NOT_FOUND)
+        {
+            std::wcerr << L"Mobile copying: Path not found\n";
+        }
+
         return 1; // Failure
     }
 }
--- syncer/syncer.cpp
+++ syncer/syncer.cpp
@@ -3,14 +3,22 @@
 #include <iostream>
 #include <string>
 #include "./copyFuncLocally.cpp"
+#include "./copyFuncMobile.cpp"
 using namespace std;
 
 int main(int argc, char const *argv[])
 {
+    //"C:\Users\Frank\Desktop\tmp.txt"
+    // need to proccess this string first to :
 
     string FileSorce = "C:\\Users\\Frank\\Desktop\\tmp.txt";
+
     string FileDistinationUSB = "D:\\tmp(USB).txt";
     copyFileLocally(FileSorce, FileDistinationUSB);
+
+    string FileDistinationMobie = "C:\\Users\\Frank\\CrossDevice\\Redmi Note 14 Pro+ 5G\\storage\\Documents\\tmp(Mobile).txt";
+    copyFileMobile(FileSorce, FileDistinationMobie);
+    //"C:\\Users\\Frank\\CrossDevice\\Redmi Note 14 Pro+ 5G\\storage\\Documents\\tmp(Mobile)";
 
     return 0;
 }
